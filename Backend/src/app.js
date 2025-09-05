fc11a86ed4816a38511c514457eb1063ab867b74
Piyush Raj <sciencetechoindia@gmail.com>
2025-09-05 11:27:58 +0530

    DB setup and CORS configurations are successful

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -6,9 +8,20 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
+
+// cors configuration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Welcome to FitTrack");
 });
 
 export default app;
